c00968b6b1290eb00a0700e478e4c8474e617216
Etoile Filante <nathanael.langlois@gmail.com>
2018-05-10 11:14:45 +0200

    Removed useless methods & updated code coverage files

--- src/Wikipediaravel.php
+++ src/Wikipediaravel.php
@@ -38,24 +38,6 @@ class Wikipediaravel
             if (FALSE === $curl)
                 throw new \Exception('Failed to initialize CURL');
 
-            switch ($method)
-            {
-                case "POST":
-                    curl_setopt($curl, CURLOPT_POST, 1);
-
-                    if ($data)
-                        curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
-                    break;
-                case "PUT":
-                    curl_setopt($curl, CURLOPT_PUT, 1);
-                    break;
-                case "DELETE":
-                    curl_setopt($curl,CURLOPT_CUSTOMREQUEST, 'DELETE');
-                    break;
-                default:
-                    if ($data)
-                        $url = sprintf("%s?%s", $url, http_build_query($data));
-            }
             curl_setopt($curl, CURLOPT_HTTPHEADER,array("Content-type: application/json"));
 
             curl_setopt($curl, CURLOPT_URL, $url);
